c2ccfce95cf22fa8807aa1841680a835c22cdc2d
yashaswashukla <yashaswashukla@gmail.com>
2022-06-23 12:58:08 +0530

    Snake-Game

---
+++ main.py
@@ -0,0 +1,63 @@
+from turtle import Screen
+import time
+from score_board import ScoreBoard
+from snake import Snake
+from food import Food
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+score_board = ScoreBoard()
+
+game_is_on = True
+score = 0
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    # detect collision with the food
+
+    if(snake.head.distance(food) < 15):
+        food.refresh()
+        snake.extend()
+        score_board.increase_score()
+
+    # Detect collision with the wall
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        score_board.game_over()
+
+    # Detect collision with it's own tail
+
+    # for segment in snake.snake:
+    #     if segment == snake.head:
+    #         pass
+    #     elif snake.head.distance(segment) < 10:
+    #         game_is_on = False
+    #         score_board.game_over()
+
+    # Slicing:
+
+    for segment in snake.snake[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score_board.game_over()
+
+
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,56 @@
+from pickle import STACK_GLOBAL
+from turtle import Turtle, right
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake():
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+
+    # Creating 3 new segments and putting them in an order
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+            # moving the snake, making the segments follow each other while only the first segment moves forward
+
+    def move(self):
+        for n in range(len(self.snake)-1, 0, -1):
+            new_X = self.snake[n-1].xcor()
+            new_Y = self.snake[n-1].ycor()
+            self.snake[n].goto(new_X, new_Y)
+        self.snake[0].forward(MOVE_DISTANCE)
+
+    def add_segment(self, position):
+        newSegments = Turtle("square")
+        newSegments.penup()
+        newSegments.color("white")
+        newSegments.goto(position)
+        self.snake.append(newSegments)
+
+    def extend(self):
+        self.add_segment(self.snake[-1].position())
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
